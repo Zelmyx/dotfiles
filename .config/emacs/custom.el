52149fe64a47c9a51aafdf503cd7bd4725e56ed3
edvin <edvin@debian>
2025-06-15 16:50:06 +0200

    add elfeed to emacs

--- .config/emacs/custom.el
+++ .config/emacs/custom.el
@@ -3,6 +3,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(elfeed-feeds
+   '("https://www.svt.se/rss.xml" "https://every.to/superorganizers/feed" "https://xkcd.com/atom.xml" "https://www.youtube.com/feeds/videos.xml?channel_id=UCwHwDuNd9lCdA7chyyquDXw" "https://www.youtube.com/feeds/videos.xml?channel_id=UC6107grRI4m0o2-emgoDnAA" "https://www.youtube.com/feeds/videos.xml?channel_id=UCHnyfMqiRRG1u-2MsSQLbXA"))
  '(org-agenda-files (list org-directory))
  '(org-directory "~/org")
  '(package-selected-packages
