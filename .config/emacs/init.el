81901f4385ea3e8c449c56b4fb9baca335fbfb6b
edvin <edvin@debian>
2025-07-26 11:43:50 +0200

    revert orderless config

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -189,27 +189,10 @@
 
 ;; Orderless configuration
 (use-package orderless
-  :after setup-minibuffer
   :ensure t
-  :demand
-  :config
-  (setq orderless-component-separator #'split-string-and-unquote)
-  (setq completion-styles '(orderless partial-completion basic))
-  (setf (alist-get ?~ orderless-affix-dispatch-alist nil 'remove) nil
-        (alist-get ?` orderless-affix-dispatch-alist) #'orderless-flex)
-
-  (defun orderless-fast-dispatch (word index total)
-    (and (= index 0) (= total 1) (length< word 5)
-       ;; `(orderless-regexp . ,(concat "^" (regexp-quote word)))
-       (cons 'orderless-literal-prefix word)))
-  
-  (orderless-define-completion-style orderless-fast
-    (orderless-style-dispatchers '(orderless-fast-dispatch
-                                   orderless-affix-dispatch))
-    (orderless-matching-styles '(orderless-literal orderless-regexp)))
-  
-  :bind (:map minibuffer-local-completion-map
-              ("SPC" . self-insert-command)))
+  :custom
+  (completion-styles '(orderless basic))
+  (completion-category-overrides '((file (styles basic partial-completion)))))
 
 (use-package marginalia
   :ensure t
