79488d53b2fd9ecbed795fd3fb02c0a7933faf11
Zelmyx <>
2025-08-03 13:18:38 +0200

    add global org-mode keybinds

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -398,6 +398,9 @@
 ;; Ensure org-mode is loaded using use-package
 (use-package org
   :ensure t
+  :bind (("C-c l" . #'org-store-link)
+         ("C-c a" . #'org-agenda)
+         ("C-c c" . #'org-capture))
   :config
 
   (custom-set-variables
