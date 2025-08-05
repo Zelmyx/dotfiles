9bdc49fbc28db99b40d9a2b3e5b9538d5f931655
Zelmyx <>
2025-08-05 20:05:33 +0200

    attempt to add norm to cdlatex-math-modify-alist

--- .config/emacs/custom.el
+++ .config/emacs/custom.el
@@ -3,6 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(cdlatex-math-modify-alist '((110 "\\left\\| \\right\\|" nil t nil nil)))
  '(org-agenda-files (list org-directory))
  '(org-directory "~/org")
  '(package-selected-packages
